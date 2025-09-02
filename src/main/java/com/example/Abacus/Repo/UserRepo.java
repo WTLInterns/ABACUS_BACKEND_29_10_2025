67b98a202c788f5ffae106ee636ca55ea3cc7368
sahil <sahildeshmukhofficial45@gmail.com>
2025-09-02 11:09:06 +0530

    add import

--- src/main/java/com/example/Abacus/Repo/UserRepo.java
+++ src/main/java/com/example/Abacus/Repo/UserRepo.java
@@ -1,8 +1,11 @@
 package com.example.Abacus.Repo;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.Abacus.Model.User;
+
 @Repository
-public interface UserRepo extends JpaRepository<User, Interger> {
+public interface UserRepo extends JpaRepository<User, Integer> {
     
 }
